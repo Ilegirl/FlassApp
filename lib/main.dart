abb903d3741efd322f0abf9147a5e47813013809
woojoovove <47964928+woojoovove@users.noreply.github.com>
2024-03-17 13:20:11 +0900

    Revert "Feat/rebase test"

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
-import 'package:flash/ui/FlashUI.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const FlashApp());
 }
 
-class FlashApp extends StatefulWidget {
+class FlashApp extends StatelessWidget {
   const FlashApp({super.key});
-  @override
-  State<FlashApp> createState() => _FlashState();
-}
 
-class _FlashState extends State<FlashApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home : Scaffold(
-            body: FlashUI(),
-            bottomNavigationBar: BottomAppBar(
-                height : 60,
-                child : Center(
-                    child : Text('Advertisement'))
-            )
-        )
+      home: Text("Flash App")
     );
   }
 }
