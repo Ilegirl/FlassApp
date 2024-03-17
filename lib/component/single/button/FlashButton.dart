7c8a629290d53f39cf32a14d2f057c8c72192d25
mugoo <anisum47@gmail.com>
2024-03-17 14:07:24 +0900

    [feat] FlashButton.dart width 300 > 200 변경

--- lib/component/single/button/FlashButton.dart
+++ lib/component/single/button/FlashButton.dart
@@ -10,7 +10,7 @@ class FlashButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 300.0,
+      width: 200.0,
       height: 100.0,
       child: OutlinedButton(
         onPressed: onPressed,
